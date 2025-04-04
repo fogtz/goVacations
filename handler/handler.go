feb47a4362f4f161a887bc61acaaf3ed5b9d750d
Leonardo Vicenzo <fogtzzzz@gmail.com>
2025-04-04 17:41:43 -0300

    update: update handlers subpackage

---
+++ handler/handler.go
@@ -0,0 +1,18 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func CreateVacancyHandler(context *gin.Context) {
+}
+
+func ShowVacancyHandler(context *gin.Context) {
+}
+
+func DeleteVacancyHandler(context *gin.Context) {
+}
+
+func UpdateVacancyHandler(context *gin.Context) {
+}
+
+func ListVacancyHandler(context *gin.Context) {
+}
--- router/routes.go
+++ router/routes.go
@@ -1,38 +1,17 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/fogtz/goVacations.git/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{
-				"message": "GET opening",
-			})
-		})
-		v1.POST("/create", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{
-				"message": "POST opening",
-			})
-		})
-		v1.DELETE("/delete", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{
-				"message": "DELETE opening",
-			})
-		})
-		v1.PUT("/update", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{
-				"message": "PUT opening",
-			})
-		})
-		v1.GET("/list", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{
-				"message": "GET openings",
-			})
-		})
+		v1.GET("/opening", handler.ShowVacancyHandler)
+		v1.POST("/create", handler.CreateVacancyHandler)
+		v1.DELETE("/delete", handler.DeleteVacancyHandler)
+		v1.PUT("/update", handler.UpdateVacancyHandler)
+		v1.GET("/list", handler.ListVacancyHandler)
 	}
 }
